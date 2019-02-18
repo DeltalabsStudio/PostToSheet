8fd4d2fe1ae08c1e08ebe3ef14ce9860038437c1
deltalabsstudio <deltalabsstudio@gmail.com>
2019-02-18 07:07:12 +0700

    first

---
+++ app/src/main/java/id/delta/posttosheet/MainActivity.java
@@ -0,0 +1,32 @@
+package id.delta.posttosheet;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new PostRequest(this).Post();
+    }
+
+}
